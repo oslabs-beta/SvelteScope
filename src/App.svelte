38c9a11563a38d559bc55691842539926134a547
Branden Basche <brandenbasche@gmail.com>
2024-03-12 15:11:52 -0400

    boilerplate complete!

--- src/App.svelte
+++ src/App.svelte
@@ -9,7 +9,7 @@
       <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
     </a>
   </div>
-  <h1>Vite + Svelte</h1>
+  <h1>Hello world!</h1>
 
   <div class="card">
     <Counter />
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,20 @@
 import './app.css'
-import App from './App.svelte'
+import App from './App.svelte';
+
+console.log('logging main.ts!')
 
 const app = new App({
   target: document.getElementById('app'),
 });
 
-chrome.devtools.panels.create(
-  'Svelte Component Explorer',
-  'src/assets/tuner.png',
-  '../devtools.html',
-  function(panel) {
-    const app = document.getElementById('app');
-    new App({ target: app });
-  }
-);
+// chrome.devtools.panels.create(
+//   'Svelte Component Explorer',
+//   'src/assets/tuner.png',
+//   '../devtools.html',
+//   function(panel) {
+//     // const app = document.getElementById('app');
+//     new App({ target: document.getElementById('app') });
+//   }
+// );
 
 export default app
