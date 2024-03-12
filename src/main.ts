d7fad865f732bdcc8772e3c48c8b2e62d58a531b
Branden Basche <brandenbasche@gmail.com>
2024-03-12 16:52:24 -0400

    added splitpane functionality for ui skeleton

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,8 @@
 import './app.css'
 import App from './App.svelte';
 
-console.log('logging main.ts!')
-
 const app = new App({
   target: document.getElementById('app'),
 });
-
-// chrome.devtools.panels.create(
-//   'Svelte Component Explorer',
-//   'src/assets/tuner.png',
-//   '../devtools.html',
-//   function(panel) {
-//     // const app = document.getElementById('app');
-//     new App({ target: document.getElementById('app') });
-//   }
-// );
 
 export default app
