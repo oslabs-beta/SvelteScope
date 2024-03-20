8e3ba4b4750e632c635e56689e44ac98be54e123
alphajames258 <james.lee4988@gmail.com>
2024-03-20 19:26:59 -0400

    MVP

--- src/lib/components/Editor.svelte
+++ src/lib/components/Editor.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-  import { onMount } from "svelte";
-  import custom_rootData_Editor from "../../stores/store-editor";
+  import { onMount } from 'svelte';
+  import custom_rootData_Editor from '../../stores/store-editor';
+  import { selectedNodeAttributes } from '../../stores/selectedNodeAttributes';
 
-  let currentData: any;
+  let currentData: any ;
   //-----------------------------------------------------------------------------------
-  import Expandable from "./Editor/Expandable.svelte";
+  import Expandable from './Editor/Expandable.svelte';
 
   // export let entries: Array<{ key: string; value: any }> = [];
   export let id: number;
@@ -18,98 +19,52 @@
       (_, error) => {
         errors[key] =
           error && error.isException
-            ? error.value.substring(0, error.value.indexOf("\n"))
+            ? error.value.substring(0, error.value.indexOf('\n'))
             : undefined;
       }
     );
   }
-  //-----------------------------------------------------------------------------------
-
-  //   function onChangeHandler(e, key){
-  //     const newVal = e.target.value;
-
-  //     // Update the value for the specific key
-  //     custom_rootData_Editor.update_rootData_Editor((data: any) => {
-  //         // Iterate over the ctx array to find the correct key and update its value
-  //         const updatedCtx = data.detail.ctx.map(ctx => {
-  //             if (Array.isArray(ctx.value)) {
-  //                 ctx.value = ctx.value.map(val => {
-  //                     // Check if the key matches the current key being updated
-  //                     if (val[key] !== undefined) {
-  //                         // Update the value for the current key
-  //                         return { ...val, [key]: newVal };
-  //                     } else {
-  //                         return val;
-  //                     }
-  //                 });
-  //             }
-  //             return ctx;
-  //         });
-
-  //         // Update the data with the modified ctx array
-  //         return { ...data, detail: { ...data.detail, ctx: updatedCtx } };
-  //     });
-  // }
-
-  // function onChangeHandler(e, ctxKey, key) {
-  //   const newVal = e.target.value;
-
-  //   // Update the value for the specific key
-  //   custom_rootData_Editor.update_rootData_Editor((data: any) => {
-  //     // Iterate over the ctx array to find the correct key and update its value
-  //     const updatedCtx = data.detail.ctx.map((ctx) => {
-  //       if (ctx.key === ctxKey && Array.isArray(ctx.value)) {
-  //         ctx.value = ctx.value.map((val) => {
-  //           // Check if the key matches the current key being updated
-  //           if (val[key] !== undefined) {
-  //             // Update the value for the current key
-  //             return { ...val, [key]: newVal };
-  //           } else {
-  //             return val;
-  //           }
-  //         });
-  //       }
-  //       return ctx;
-  //     });
-
-  //     // Update the data with the modified ctx array
-  //     return { ...data, detail: { ...data.detail, ctx: updatedCtx } };
-  //   });
-  // }
-
+  
   onMount(() => {
-    const unsubscribe = custom_rootData_Editor.subcribe_rootData_Editor(
-      (data: any) => {
-        currentData = data;
-        console.log("Data from rootData_Editor: ", currentData);
-      }
-    );
+    // const unsubscribe = custom_rootData_Editor.subcribe_rootData_Editor(
+    //   (data: any) => {
+    //     currentData = data;
+    //     console.log('Data from rootData_Editor: ', currentData);
+    //     id = currentData.id;
+    //   }
+    // );
 
-    return unsubscribe; // Cleanup subscription when component unmounts
+    // return unsubscribe; // Cleanup subscription when component unmounts
+    const unsubscribe = selectedNodeAttributes.subscribe((data: any) => {
+    currentData = data;
+    console.log('Data from selectedNodeAttributes PIZZA: ', currentData);
+    id = currentData.id;
+
+  });
+  return unsubscribe
+
   });
 </script>
 
-
 <h2>detail.attributes</h2>
 {#if currentData && currentData.type === 'component'}
-	<ul>
-		{#each currentData.detail.attributes as { key, value } (key)}
-			<Expandable
-				{readonly}
-				{key}
-				{value}
-				error={errors[key]}
-				on:change={(e) => change(key, e.detail)}
-			/>
-		{/each}
-	</ul>
+  <ul>
+    {#each currentData.detail.attributes as { key, value } (key)}
+      <Expandable
+        {readonly}
+        {key}
+        {value}
+        error={errors[key]}
+        on:change={(e) => change(key, e.detail)}
+      />
+    {/each}
+  </ul>
 {:else}
-	<div style:padding-left="1rem" style:color="rgb(118, 118, 118)">None</div>
+  <div style:padding-left="1rem" style:color="rgb(118, 118, 118)">None</div>
 {/if}
 
-
 {#if currentData}
-<h2>Ctx.value - Array</h2>
+  <h2>Ctx.value - Array</h2>
   {#each currentData.detail.ctx as ctx}
     {#if Array.isArray(ctx.value)}
       <p>Key {ctx.key}</p>
@@ -124,49 +79,12 @@
             error={errors[key]}
             on:change={(e) => change(key, e.detail)}
           />
-          <!-- <p><span style="color: red">{key}:</span> {value}</p>
-          <div>
-            <label for={key}>{key}</label>
-            <input type="text" id={key} on:input={onChangeHandler}/>
-            
-          </div> -->
+        
         {/each}
-        <!-- {#each Object.entries(val) as [key, value] (key)}
-          <div>
-            <label for={key}>{key}</label>
-            <input
-              type="text"
-              id={key}
-              {value}
-              on:input={(e) => onChangeHandler(e, key)}
-            />
-          </div>
-        {/each} -->
-
-        <!-- 
-        {#each Object.entries(val) as [key, value]}
-          <div>
-            <label for={ctx.key + "-" + key}>{key}</label>
-            <input
-              type="text"
-              id={ctx.key + "-" + key}
-              {value}
-              on:input={(e) => onChangeHandler(e, ctx.key, key)
-              }
-            />
-          </div>
-        {/each} -->
+       
+      
       {/each}
     {/if}
   {/each}
 {/if}
 
-<!-- {#each entries as { key, value } (key)}
-  <Expandable
-    {readonly}
-    {key}
-    {value}
-    error={errors[key]}
-    on:change={(e) => change(key, e.detail)}
-  />
-{/each} -->
--- src/lib/components/Tree.svelte
+++ src/lib/components/Tree.svelte
@@ -2,6 +2,7 @@
   import { onMount } from 'svelte';
   import * as d3 from 'd3';
   import RootComponentStore from '../../stores/Store';
+ import { selectedNodeAttributes } from '../../stores/selectedNodeAttributes';
   import { stringify } from 'querystring';
   import Popup from '../../../static/Popup/Popup.svelte';
 
@@ -37,6 +38,21 @@
       }
       return componentData;
     }
+  }
+  function handleMouseOver() {
+    d3.select(this)
+      .select('rect')
+      .transition()
+      .delay(100)
+      .attr('fill', 'orangered');
+  }
+
+  function handleMouseOut() {
+    d3.select(this)
+      .select('rect')
+      .transition()
+      .delay(100)
+      .attr('fill', 'orange');
   }
 
   // Function to update the tree
@@ -85,7 +101,8 @@
       .enter()
       .append('g')
       .attr('class', 'node')
-      .attr('transform', (node) => `translate(${node.x},${node.y})`);
+      .attr('transform', (node) => `translate(${node.x},${node.y})`)
+      .on('click', handleNodeClick)
 
     // Append rectangle for nodes
     nodes
@@ -103,6 +120,8 @@
       .attr('text-anchor', 'middle')
       .attr('dy', '15')
       .text((d) => d.data.tagName);
+
+    nodes.on('mouseover', handleMouseOver).on('mouseout', handleMouseOut);
 
     svg.selectAll('.link').attr('fill', 'none').attr('stroke', 'black');
     //changing font size of text
@@ -123,6 +142,16 @@
   function dragended(event, d) {
     d3.select(this).classed('active', false);
   }
+  function handleNodeClick(event, d) {
+  // Access data associated with the clicked node
+  const clickedNodeData = d.data;
+  selectedNodeAttributes.set(clickedNodeData);
+
+  // Logic to display specific props for the clicked node
+
+  console.log('Clicked node data:', clickedNodeData); // For debugging
+  
+}
 
   onMount(updateTree);
 </script>
