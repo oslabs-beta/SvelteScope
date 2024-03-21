34b36c7ad1d8940483ce5839c3dede5c196bb8c4
Branden Basche <brandenbasche@gmail.com>
2024-03-21 15:26:52 -0400

    removed unused code

--- src/stores/Store.js
+++ src/stores/Store.js
@@ -9,15 +9,12 @@ export const SvelteVersionStore = writable();
 //store for props
 const rootData_Editor = writable({});
 
-export const custom_rootData_Editor = {
-  subcribe_rootData_Editor: rootData_Editor.subscribe,
-  set_rootData_Editor: (data) => {
-    rootData_Editor.set(data);
-  },
-  update_rootData_Editor: (updater) => {
-    rootData_Editor.update(updater);
-  },
-};
-
 //Selected Node Store
 export const selectedNodeAttributes = writable({});
+
+
+
+const globalStore = {
+
+}
+
