bb0008bd74e5f55317fb144bb651d649075f353f
binhnguyen96html <binhnguyenbiz96@gmail.com>
2024-03-20 20:08:07 -0400

    try pushing

--- src/lib/components/Editor.svelte
+++ src/lib/components/Editor.svelte
@@ -4,7 +4,8 @@
 
   let currentData: any;
   //-----------------------------------------------------------------------------------
-  import Expandable from "./Editor/Expandable.svelte";
+  // import Expandable from "./Editor/Expandable.svelte";
+  import Props from "./Editor/Props.svelte";
 
   // export let entries: Array<{ key: string; value: any }> = [];
   export let id: number;
@@ -12,78 +13,13 @@
 
   const errors: Record<string, string | undefined> = {};
 
-  function change(key: string, value: any) {
-    chrome.devtools.inspectedWindow.eval(
-      `__svelte_devtools_inject_state(${id}, '${key}', ${value})`,
-      (_, error) => {
-        errors[key] =
-          error && error.isException
-            ? error.value.substring(0, error.value.indexOf("\n"))
-            : undefined;
-      }
-    );
-  }
-  //-----------------------------------------------------------------------------------
-
-  //   function onChangeHandler(e, key){
-  //     const newVal = e.target.value;
-
-  //     // Update the value for the specific key
-  //     custom_rootData_Editor.update_rootData_Editor((data: any) => {
-  //         // Iterate over the ctx array to find the correct key and update its value
-  //         const updatedCtx = data.detail.ctx.map(ctx => {
-  //             if (Array.isArray(ctx.value)) {
-  //                 ctx.value = ctx.value.map(val => {
-  //                     // Check if the key matches the current key being updated
-  //                     if (val[key] !== undefined) {
-  //                         // Update the value for the current key
-  //                         return { ...val, [key]: newVal };
-  //                     } else {
-  //                         return val;
-  //                     }
-  //                 });
-  //             }
-  //             return ctx;
-  //         });
-
-  //         // Update the data with the modified ctx array
-  //         return { ...data, detail: { ...data.detail, ctx: updatedCtx } };
-  //     });
-  // }
-
-  // function onChangeHandler(e, ctxKey, key) {
-  //   const newVal = e.target.value;
-
-  //   // Update the value for the specific key
-  //   custom_rootData_Editor.update_rootData_Editor((data: any) => {
-  //     // Iterate over the ctx array to find the correct key and update its value
-  //     const updatedCtx = data.detail.ctx.map((ctx) => {
-  //       if (ctx.key === ctxKey && Array.isArray(ctx.value)) {
-  //         ctx.value = ctx.value.map((val) => {
-  //           // Check if the key matches the current key being updated
-  //           if (val[key] !== undefined) {
-  //             // Update the value for the current key
-  //             return { ...val, [key]: newVal };
-  //           } else {
-  //             return val;
-  //           }
-  //         });
-  //       }
-  //       return ctx;
-  //     });
-
-  //     // Update the data with the modified ctx array
-  //     return { ...data, detail: { ...data.detail, ctx: updatedCtx } };
-  //   });
-  // }
-
   onMount(() => {
     const unsubscribe = custom_rootData_Editor.subcribe_rootData_Editor(
       (data: any) => {
         currentData = data;
         console.log("Data from rootData_Editor: ", currentData);
 
-        id = currentData.id
+        id = currentData.id;
       }
     );
 
@@ -91,84 +27,38 @@
   });
 </script>
 
+<main>
+  <!-- //TYPE: COMPONENT----------------------------------------------------------- -->
+  {#if currentData && currentData.type === "component"}
+    <h2>Props - currentData.detail.attributes</h2>
+    <Props id={currentData.id} currentProps={currentData.detail.attributes} />
+    <hr />
 
-<h2>detail.attributes</h2>
-{#if currentData && currentData.type === 'component'}
-	<ul>
-		{#each currentData.detail.attributes as { key, value } (key)}
-			<Expandable
-				{readonly}
-				{key}
-				{value}
-				error={errors[key]}
-				on:change={(e) => change(key, e.detail)}
-			/>
-		{/each}
-	</ul>
-{:else}
-	<div style:padding-left="1rem" style:color="rgb(118, 118, 118)">None</div>
-{/if}
+    <h2>Events</h2>
+    <hr />
 
+    <h2>State - currentData.detail.ctx</h2>
+    <Props id={currentData.id} currentProps={currentData.detail.ctx} />
 
-{#if currentData}
-<h2>Ctx.value - Array</h2>
-  {#each currentData.detail.ctx as ctx}
-    {#if Array.isArray(ctx.value)}
-      <p>Key {ctx.key}</p>
-      <hr />
-      {#each ctx.value as val}
-        <hr />
-        {#each Object.entries(val) as [key, value]}
-          <Expandable
-            {readonly}
-            {key}
-            {value}
-            error={errors[key]}
-            on:change={(e) => change(key, e.detail)}
-          />
-          <!-- <p><span style="color: red">{key}:</span> {value}</p>
-          <div>
-            <label for={key}>{key}</label>
-            <input type="text" id={key} on:input={onChangeHandler}/>
-            
-          </div> -->
-        {/each}
-        <!-- {#each Object.entries(val) as [key, value] (key)}
-          <div>
-            <label for={key}>{key}</label>
-            <input
-              type="text"
-              id={key}
-              {value}
-              on:input={(e) => onChangeHandler(e, key)}
-            />
-          </div>
-        {/each} -->
+    <!-- //TYPE: BLOCK AND ITERATION----------------------------------------------------------- -->
+  {:else if (currentData && currentData.type === "block") || currentData && currentData.type === "iteration"}
+    <h2>State</h2>
+    <Props
+      readonly
+      id={currentData.id}
+      currentProps={currentData.detail.attributes}
+    />
 
-        <!-- 
-        {#each Object.entries(val) as [key, value]}
-          <div>
-            <label for={ctx.key + "-" + key}>{key}</label>
-            <input
-              type="text"
-              id={ctx.key + "-" + key}
-              {value}
-              on:input={(e) => onChangeHandler(e, ctx.key, key)
-              }
-            />
-          </div>
-        {/each} -->
-      {/each}
-    {/if}
-  {/each}
-{/if}
+    <!-- //TYPE: ELEMENT----------------------------------------------------------- -->
+  {:else if currentData && currentData.type === "element"}
+    <h2>Attributes</h2>
+    <Props
+      readonly
+      id={currentData.id}
+      currentProps={currentData.detail.attributes}
+    />
 
-<!-- {#each entries as { key, value } (key)}
-  <Expandable
-    {readonly}
-    {key}
-    {value}
-    error={errors[key]}
-    on:change={(e) => change(key, e.detail)}
-  />
-{/each} -->
+    <h2>Events</h2>
+    <!-- <Props id={currentData.id} currentProps={events} /> -->
+  {/if}
+</main>
---
+++ src/lib/components/Editor/Props.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+  import Expandable from "./Expandable.svelte";
+
+  // export let currentData: Array<{ key: string; value: any }> = [];
+  export let currentProps: any = [];
+
+  export let id: number;
+  export let readonly = false;
+
+  const errors: Record<string, string | undefined> = {};
+  function change(key: string, value: any) {
+    chrome.devtools.inspectedWindow.eval(
+      `__svelte_devtools_inject_state(${id}, '${key}', ${value})`,
+      (_, error) => {
+        errors[key] =
+          error && error.isException
+            ? error.value.substring(0, error.value.indexOf("\n"))
+            : undefined;
+      }
+    );
+  }
+</script>
+
+{#if currentProps.length}
+  <ul>
+    {#each currentProps as { key, value } (key)}
+      <Expandable
+        {readonly}
+        {key}
+        {value}
+        error={errors[key]}
+        on:change={(e) => change(key, e.detail)}
+      />
+    {/each}
+  </ul>
+{:else}
+  <div style:padding-left="1rem" style:color="rgb(118, 118, 118)">None</div>
+{/if}
+
+<style>
+  ul {
+    display: grid;
+    gap: 0.25rem;
+    padding-left: 1rem;
+    font-size: 0.75rem;
+  }
+</style>
