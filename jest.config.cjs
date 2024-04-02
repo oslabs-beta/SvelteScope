ee4f92e4f15aed434651dc9ffc18b75306c656bf
binhnguyen96html <binhnguyenbiz96@gmail.com>
2024-04-02 13:38:28 -0400

    add svelte test

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,9 +2,13 @@
 module.exports = {
   preset: 'ts-jest/presets/default-esm',
   testEnvironment: 'node',
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node', 'svelte'],
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
+    "^.+\\.svelte$": "svelte-jester"
   },
-  testMatch: ['**/*.test.(ts|tsx|js|jsx)']
+  testMatch: ['**/*.test.(ts|tsx|js|jsx)'],
+  extensionsToTreatAsEsm: [".svelte"],
+  testEnvironment: "jsdom",
+  setupFilesAfterEnv: ["<rootDir>/jest-setup.js"]
 };
